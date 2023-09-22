e132a03b98ab1756527568742051e0d3e5b149c9
CMEND5210 <cmend5210@stu.hostos.cuny.edu>
2023-09-21 20:08:15 -0400

    Week 4 Update

--- New Unity Project/Assets/Script/BasicMovementP1.cs
+++ New Unity Project/Assets/Script/BasicMovementP1.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class BasicMovementP1 : MonoBehaviour
 {
+    public GameObject hazard;
+    public GameObject[] hazards;
+    Vector3 startPos;
+    public AudioClip death;
+    AudioSource audPlayer;
 
     void Start()
     {
-
+        startPos = transform.position;
+        audPlayer = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -53,6 +59,15 @@ public class BasicMovementP1 : MonoBehaviour
             Debug.Log("The D Key was pressed.");
 
             transform.position += Vector3.back;
+        }
+
+        for (int i = 0; i < hazards.Length; i++)
+        {
+            if (transform.position == hazards[i].transform.position)
+            {
+                transform.position = startPos;
+                audPlayer.PlayOneShot(death, 0.75F);
+            }
         }
     }
 }
